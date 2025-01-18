2b512f1df3ae13107cefbd06d7d2439acdd2dfce
lucassebastianfiorio <86678823+lucassebastianfiorio@users.noreply.github.com>
2025-01-18 11:09:42 -0300

    V1

--- app.js
+++ app.js
@@ -1,1 +1,41 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let listaAmigos = [];
+let cantidadAmigos = 0;
+
+function agregarAmigo() {
+    let nombre = document.getElementById('amigo').value;
+
+    if (nombre === '') {
+        alert('Ingrese un nombre');
+    } else{
+        listaAmigos.push(nombre);
+    document.getElementById('amigo').value = '';
+    cantidadAmigos++;
+    console.log(`lista de amigos: ${listaAmigos}`);
+    console.log(`cantidad de amigos: ${cantidadAmigos}`);
+    mostrarListaAmigos();
+    }
+    
+}
+
+function mostrarListaAmigos(){
+    let lista = document.getElementById('listaAmigos');
+    lista.innerHTML = '';
+    listaAmigos.forEach((amigo) => {
+        let li = document.createElement('li');
+        li.textContent = amigo;
+        lista.appendChild(li);
+    });
+}
+
+function sortearAmigo(){
+    indiceAmigo = Math.floor(Math.random() * cantidadAmigos);
+    let amigoSorteado = listaAmigos[indiceAmigo];
+    let resultado = document.getElementById('resultado');
+    ocultarLista();
+    resultado.innerHTML = `El ganador es: ${amigoSorteado}`;
+  
+}
+
+function ocultarLista(){
+    document.querySelector('#listaAmigos').classList.add('oculto');
+}
